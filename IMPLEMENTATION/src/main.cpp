fe808c4055585c4ed32e02cfb8f2918541e7b5b5
MCLEANS <jackagwaya@gmail.com>
2020-08-01 18:30:43 +0300

    Timer1 test implementation

--- IMPLEMENTATION/src/main.cpp
+++ IMPLEMENTATION/src/main.cpp
@@ -10,11 +10,30 @@
 
 
 #include "stm32f10x.h"
+#include "Timerconfiguration.h"
 
-			
+#define PRESCALER 65535
+#define ARR_VALUE 1098
+custom_libraries::Timer_configuration timer(TIM1,PRESCALER,ARR_VALUE);
+
+uint32_t counter = 0;
+
+//IMPLEMENT INTERRUPT HANDLER
+extern "C" void TIM1_UP_IRQHandler(void){
+	//CHECK THAT WE ARE HERE BECAUSE OF TIMER1 UPDATE EVENT
+	if(TIM1->SR & TIM_SR_UIF){
+		TIM1->SR &= ~TIM_SR_UIF;
+		counter++;
+	}
+
+}
 
 int main(void)
 {
+	timer.initialize();
+
+	NVIC_SetPriority(TIM1_UP_IRQn,0x03);
+	NVIC_EnableIRQ(TIM1_UP_IRQn);
 
 	while(1){
 
